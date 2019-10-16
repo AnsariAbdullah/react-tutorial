2d4edf530f59de6cbe0e6e15ca14cf3aead244d7
Abdullah <ansariabdullahmar@gmail.com>
2019-10-16 06:59:55 +0530

    add setState

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,15 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     count: 0,
-  //     tags: ["tag1", "tag2", "tag3"]
-  //   }
-
-  //   this.handleClick = this.handleClick.bind(this)
-  // }
   state = {
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   };
 
   handleClick = () => {
-    // this.setState({
-    //   count = this.state.count + 1
-    // })
-    console.log("this.state.count", this)
+    this.setState({
+      count: this.state.count + 1
+    })
   }
 
   render() {
