607e8594e1913d20271d4d9290d8093470473271
Abdullah <ansariabdullahmar@gmail.com>
2019-11-12 00:55:42 +0530

    add handleReset

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ]
   }
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    })
+    this.setState({counters})
+  }
+
   handleDelete = (counterID) => {
     console.log('Event handler called', counterID);
     const counters = this.state.counters.filter(c => c.id !== counterID)
@@ -23,6 +31,13 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
