bd12e138aa311d2fb11568a157ef51c8654863b3
Abdullah <ansariabdullahmar@gmail.com>
2019-12-05 08:58:00 +0530

    lifting state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import './index.css';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
 ReactDOM.render(<App />, document.getElementById('root'));
