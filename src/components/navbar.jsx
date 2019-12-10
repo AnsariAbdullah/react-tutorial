5c1e970fa215d90966009d3c7e80c18781c8404e
Abdullah <ansariabdullahmar@gmail.com>
2019-12-10 08:00:06 +0530

    making nav component stateless function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-	render() { 
-		return (
-			<nav className="navbar navbar-light bg-light">
-				<a className="navbar-brand" href="google.com">
-					Navbar
-					<span className="badge badge-pill badge-secondry">
-						{this.props.totalCounters}
-					</span>
-				</a>
-			</nav>
-		);
-	}
+const NavBar = () => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<a className="navbar-brand" href="google.com">
+				Navbar
+				<span className="badge badge-pill badge-secondry">
+					{this.props.totalCounters}
+				</span>
+			</a>
+		</nav>
+	);
 }
+
  
 export default NavBar;
