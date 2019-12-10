1cea30289bde166ee7e51f83301c7179d938a62e
Abdullah <ansariabdullahmar@gmail.com>
2019-12-10 08:01:40 +0530

    passing props in function

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-const NavBar = () => {
+const NavBar = (props) => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<a className="navbar-brand" href="google.com">
 				Navbar
 				<span className="badge badge-pill badge-secondry">
-					{this.props.totalCounters}
+					{props.totalCounters}
 				</span>
 			</a>
 		</nav>
